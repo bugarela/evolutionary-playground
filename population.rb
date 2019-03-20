d549e427504505ca219249dfa8971c4068b24598
Gabriela Moreira <gabriela@magrathealabs.com>
2019-03-20 11:16:54 -0300

    ::white_check_mark: Add specs and fix populations

--- population.rb
+++ population.rb
@@ -11,47 +11,59 @@ class Population
       puts dimensionality.join ' '
     end
   end
+
+  class Binary < Population
+    def initialize(size, dimensionality)
+      super
+    end
+
+    def individuals
+      @size.times.collect { @dimensionality.times.collect { rand(0..1) } }
+    end
+  end
+
+  class Integer < Population
+    def initialize(size, dimensionality, bounds)
+      @bounds = bounds.each { |key, bound| bounds[key] = bound.to_i }
+      super(size, dimensionality)
+    end
+
+    def individuals
+      @size.times.collect { @dimensionality.times.collect { rand(@bounds[:lower]..@bounds[:upper]) } }
+    end
+  end
+
+  class IntegerPermutation < Population
+    def initialize(size, dimensionality, bounds)
+      @bounds = bounds
+      super(size, dimensionality)
+    end
+
+    def individuals
+      @size.times.collect do
+        integers = (Array @bounds[:lower]..@bounds[:upper]).shuffle
+        integers.take(@dimensionality)
+      end
+    end
+  end
+
+  class Real < Population
+    def initialize(size, dimensionality, bounds)
+      @bounds = bounds.each { |key, bound| bounds[key] = bound.to_f }
+      super(size, dimensionality)
+    end
+
+    def individuals
+      @size.times.collect { @dimensionality.times.collect { rand(@bounds[:lower]..@bounds[:upper]) } }
+    end
+  end
 end
 
-class Population::Binary < Population
-  def initialize(size, dimensionality)
-    super
-  end
-
-  def individuals
-    @size.times.collect { @dimensionality.times.collect { rand(0..1) } }
-  end
-end
-
-class Population::Integer < Population
-  def initialize(size, dimensionality, bounds)
-    @bounds = bounds
-    super(size, dimensionality)
-  end
-
-  def individuals
-    @size.times.collect { @dimensionality.times.collect { rand(@bounds[:lower]..@bounds[:upper]) } }
-  end
-end
-
-class Population::IntegerPermutation < Population
-  def initialize(size, dimensionality, bounds)
-    @bounds = bounds
-    super(size, dimensionality)
-  end
-
-  def individuals
-    integers = (Array @bounds[:lower]..@bounds[:upper]).shuffle
-    integers.take(@dimensionality)
-  end
-
-  def show
-    puts individuals.join ' '
-  end
-end
-
-Population::Binary.new(10, 10).show
-puts '----'
-Population::Integer.new(10, 10, lower: 0, upper: 3).show
-puts '----'
-Population::IntegerPermutation.new(10, 10, lower: 0, upper: 10).show
+# Population::Binary.new(10, 10).show
+# puts '----'
+# Population::Integer.new(10, 10, lower: 0, upper: 3).show
+# puts '----'
+# Population::IntegerPermutation.new(10, 10, lower: 0, upper: 10).show
+# puts '----'
+# Population::Real.new(10, 10, lower: 0, upper: 3).show
+# puts '----'
---
+++ spec/population_spec.rb
@@ -0,0 +1,110 @@
+require_relative '../population'
+
+RSpec.describe Population do
+  let(:population_size) { 10 }
+  let(:dimensionality) { 20 }
+  let(:bounds) do
+    {
+      lower: -1,
+      upper: 23.1,
+    }
+  end
+
+  shared_examples_for 'a successful generation' do
+    it 'has the right number of individuals' do
+      expect(population.individuals.length).to eq(population_size)
+    end
+
+    it 'has right sized chromossomes' do
+      population.individuals.each do |chromossome|
+        expect(chromossome.length).to eq(dimensionality)
+      end
+    end
+  end
+
+  describe 'a binary population' do
+    let(:population) { Population::Binary.new(population_size, dimensionality) }
+
+    it_behaves_like 'a successful generation'
+
+    it 'has only binary genes' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to eq(0).or eq(1)
+        end
+      end
+    end
+  end
+
+  describe 'a integer population' do
+    let(:population) { Population::Integer.new(population_size, dimensionality, bounds) }
+
+    it_behaves_like 'a successful generation'
+
+    it 'has only integer genes' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to be_a(Integer)
+        end
+      end
+    end
+
+    it 'respects the bounds' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to be_between(bounds[:lower], bounds[:upper])
+        end
+      end
+    end
+  end
+
+  describe 'a integer permutation population' do
+    let(:population) { Population::IntegerPermutation.new(population_size, dimensionality, bounds) }
+
+    it_behaves_like 'a successful generation'
+
+    it 'has only integer genes' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to be_a(Integer)
+        end
+      end
+    end
+
+    it 'respects the bounds' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to be_between(bounds[:lower], bounds[:upper])
+        end
+      end
+    end
+
+    it 'has permutated values on all chromossomes' do
+      population.individuals.each do |chromossome|
+        expect(chromossome.uniq.length).to eq(dimensionality)
+      end
+    end
+  end
+
+  describe 'a real population' do
+    let(:population) { Population::Real.new(population_size, dimensionality, bounds) }
+
+    it_behaves_like 'a successful generation'
+
+    it 'has only real genes' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to be_a(Float)
+        end
+      end
+    end
+
+    it 'respects the bounds' do
+      population.individuals.each do |chromossome|
+        chromossome.each do |gene|
+          expect(gene).to be_between(bounds[:lower], bounds[:upper])
+        end
+      end
+    end
+  end
+end
