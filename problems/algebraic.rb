773ad4b4d541412f1bf47e7bc0030e583f0ebe99
Gabriela Moreira <gabriela@magrathealabs.com>
2019-03-24 21:26:45 -0300

    :sparkles: add algebraic function evaluation

---
+++ problems/algebraic.rb
@@ -0,0 +1,56 @@
+require_relative 'problem'
+require_relative '../population'
+
+class Algebraic < Problem
+  def initialize(population_size, dimensionality)
+    super(population_size)
+    @dimensionality = dimensionality
+    @population = generate_population
+  end
+
+  attr_reader :population
+
+  def generate_population
+    Population::Binary.new(@population_size, @dimensionality)
+  end
+
+  def evaluate(individual)
+    translated = translate(individual)
+    return function(translated) + 4
+  end
+
+  def translate(individual)
+    decoded = decode(individual)
+    adjust_scale(decoded)
+  end
+
+  def best
+    raw = @population.individuals.sort_by { |individual| evaluate(individual) }.last
+    best = translate(raw)
+    return best, function(best)
+  end
+
+  def worst
+    raw = @population.individuals.sort_by { |individual| evaluate(individual) }.first
+    best = translate(raw)
+    return best, function(best)
+  end
+
+  private
+
+  def function(number)
+    Math::cos(20*number) - number.abs / 2 + number ** 3 / 4
+  end
+
+  def decode(binary)
+    integer = binary.first(2).inject { |a,b| a.to_s + b.to_s }.to_i(2)
+    decimal = binary.last(14).inject { |a,b| a.to_s + b.to_s }.to_i(2)
+    return integer + (decimal.to_f % 10000) / 10000
+  end
+
+  def adjust_scale(number)
+    return number - 2
+  end
+end
+
+# puts Algebraic.new(1000, 16).best
